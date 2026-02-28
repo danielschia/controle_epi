70ad78c6ad54557fa711a9ac2861b08f0691a80b
danielschia <daniel.schia@gmail.com>
2026-02-28 15:41:28 -0300

    testes refatorados

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-@pytest.fixture(autouse=True)
-def isolate_media_root(tmp_path, settings):
-    """Ensure MEDIA_ROOT is isolated for tests so uploads don't pollute the repo."""
-    settings.MEDIA_ROOT = tmp_path / "media"
